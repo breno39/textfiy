972d22d9cd94290dc1489072a8b8abecdf9c8a80
breno39 <64810645+breno39@users.noreply.github.com>
2023-02-07 11:06:13 -0300

    first commit

---
+++ src/main/java/com/infotek/textify/TextifyApplication.java
@@ -0,0 +1,13 @@
+package com.infotek.textify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TextifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/infotek/textify/TextifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infotek.textify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
